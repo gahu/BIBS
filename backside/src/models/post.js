b86c8a5d988a20ae1dfe2bec23b66ecba866cf19
gahu <bgg8988@naver.com>
2018-09-21 17:15:59 +0900

    python code upload & how to use backside

---
+++ backside/src/models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const Post = new Schema({
+    title: String,
+    body: String,
+    tags: [String], // 문자열 배열
+    publishedDate: {
+        type: Date,
+        default: new Date() // 현재 날짜를 기본 값으로 설정
+    }
+});
+
+// 모델 생성
+module.exports = mongoose.model('Post', Post); // 스키마 이름, 스키마 객체
